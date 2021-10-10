3782db04d17916f5d5ab6a289ed8b6ca66c3c828
ashwith31 <ashwitash@gmail.com>
2021-10-11 00:04:32 +0530

    Added: Created database connection and added model and dto classes

---
+++ src/main/java/com/example/EmployeePayrollApp/controller/EmployeePayrollController.java
@@ -0,0 +1,7 @@
+package com.example.EmployeePayrollApp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeePayrollController {
+}
---
+++ src/main/java/com/example/EmployeePayrollApp/dto/EmployeeDto.java
@@ -0,0 +1,10 @@
+package com.example.EmployeePayrollApp.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDto {
+
+    private String name;
+    private int salary;
+}
